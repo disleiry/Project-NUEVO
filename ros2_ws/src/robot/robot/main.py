63cf14f33c7299d581ea45571d8513bda562f66d
oscarschacht <oscarschacht@g.ucla.edu>
2026-05-05 16:47:36 -0700

    Update main.py

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -118,9 +118,8 @@ def run(robot: Robot) -> None:
 
         elif state == "MOVING":
             show_moving_leds(robot)
-            robot.set_velocity(100, 0)
-            pose = robot._pose
-            print("POSE:", pose)
+            
+            print("PATH LEN:", len(robot._obstacle_avoidance_planner.remaining_path))
             # if next_tick % 0.5 < period: # print every half second
             #     robot._draw_lidar_obstacles()
             #     print("Obstacle figure updated.")
