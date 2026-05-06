c3bf5f6053af7b1464126e095333d67b8de0413f
oscarschacht <oscarschacht@g.ucla.edu>
2026-05-05 17:43:31 -0700

    Update main.py

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -82,8 +82,8 @@ GPS_TANGENT_MIN_DISPLACEMENT_MM = 100.0
 PATH_CONTROL_POINTS = [
      (0.0,  0.0), # start
      (0.0, 3250), # Waypoint 1
-     (350, 3250), # Waypoint 2
-     (350, 0.0),
+     (290, 3250), # Waypoint 2
+     (290, 0.0),
      (0.0, 0.0), # Return to start
 
 ]
