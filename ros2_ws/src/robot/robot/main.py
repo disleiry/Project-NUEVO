7ed2997144bf3d0d4f2d4fc7242cbb0b06888792
oscarschacht <oscarschacht@g.ucla.edu>
2026-05-20 17:01:16 -0700

    Update main.py

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -102,8 +102,8 @@ ENABLE_STOP_SIGN_OVERRIDE = False
 
 PURE_PURSUIT_CONTROL_POINTS = [
     #(0.0, 0.0),        # start
-    (300.0, 3250.0),      # Waypoint 1: home straight
-    (920.0, 3250.0),    # Waypoint 2: transition / turn
+    (300.0, 3400.0),      # Waypoint 1: home straight
+    (920.0, 3400.0),    # Waypoint 2: transition / turn
     (920.0, 700.0),     # Waypoint 3: ramp / return direction
     (1800.0, 700.0),    # Waypoint 4: entrance toward obstacle course
 ]
