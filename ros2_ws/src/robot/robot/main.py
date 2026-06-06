fad655dc19e0833eeed6f6c5bbae95960a2a979e
oscarschacht <oscarschacht@g.ucla.edu>
2026-06-06 11:39:41 -0700

    Update main.py

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -1173,7 +1173,7 @@ def run(robot: Robot) -> None:
             if approach_mm > 0.0:
                 robot.move_backward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
             elif approach_mm < 0.0:
-                robot.move_forward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
+                robot.move_forward(-approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
 
             robot.turn_by(-(81 + angle_corr), TURN_VELOCITY_DEG, tolerance_deg=TURN_TOLERANCE_DEG, blocking=True)
 
@@ -1201,7 +1201,7 @@ def run(robot: Robot) -> None:
             if approach_mm > 0.0:
                 robot.move_backward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
             elif approach_mm < 0.0:
-                robot.move_forward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)            
+                robot.move_forward(-approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)            
                 
             robot.turn_by(-(81 + angle_corr), TURN_VELOCITY_DEG, tolerance_deg=TURN_TOLERANCE_DEG, blocking=True)
 
@@ -1229,7 +1229,7 @@ def run(robot: Robot) -> None:
             if approach_mm > 0.0:
                 robot.move_backward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
             elif approach_mm < 0.0:
-                robot.move_forward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
+                robot.move_forward(-approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
                 
             robot.turn_by(-(81 + angle_corr), TURN_VELOCITY_DEG, tolerance_deg=TURN_TOLERANCE_DEG, blocking=True)
 
@@ -1260,7 +1260,7 @@ def run(robot: Robot) -> None:
             if approach_mm > 0.0:
                 robot.move_backward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
             elif approach_mm < 0.0:
-                robot.move_forward(approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
+                robot.move_forward(-approach_mm, APPROACH_VELOCITY, POS_TOLERANCE_MM, blocking=True)
                 
             robot.turn_by(-(81 + angle_corr), TURN_VELOCITY_DEG, tolerance_deg=TURN_TOLERANCE_DEG, blocking=True)
 
